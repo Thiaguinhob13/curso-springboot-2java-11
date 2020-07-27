088beda3c88e1e342ecb666d301011d10c611481
Thiago melo <Thiaguinhob13>
2020-07-27 19:37:59 -0300

    Project created

---
+++ src/test/java/com/donamoca/bijuteria/DonamocabijuteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donamoca.bijuteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonamocabijuteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
